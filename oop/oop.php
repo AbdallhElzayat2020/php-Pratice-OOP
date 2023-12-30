6958746ef18de5a284096c73de343bd64fcc303b
AbdallhElzayat2020 <https://github.com/AbdallhElzayat2020/E-commerce-React-App>
2023-12-31 01:17:29 +0200

    zz

--- oop/oop.php
+++ oop/oop.php
@@ -1,28 +1,78 @@
 <?php 
-// object orintted Programming
-// مجموعة من الكائنات لها علاقة مع بعضها  لو مش فاهم  المعني الانجلش 
+// Object-Oriented Programming
+// مجموعة من الكائنات لها علاقة مع بعضها
 
-
-// class  - object
-
-class Persone {
-  public $name;
-  public  $age;
-  public  $sallery;
-  public  $overTime;
-  public  $rateOverTime;
-  public $appsent;
-  public $rateAppsent;
+// class - object
+class Person {
+  public $user_Name;
+  public $user_email;
+  public $age;
   public $tax;
+  public $absent;
+  public $rateAbsent;
+  public $overTime;
+  public $rateOverTime;
+
+  public function calcOverTime() 
+  {
+    return $this->overTime * $this->rateOverTime;
+  }
+  public function getAge($age2) {
+  if($age2<12) {
+    echo("age less Than 12 ");
+  }else {
+  $this->age=$age2;
+  }
+  }
 }
+ 
+// declare new user
+$ahmed = new Person;
 
-$ahmed=new Persone();
-$abdo=new Persone();
+$ahmed->overTime = 30;
+$ahmed->rateOverTime = 2;
 
-// calc rateOverTime
-function calcOverTime($overTime,$rateOverTime) {
-return $overTime*$rateOverTime;
+echo $ahmed->calcOverTime();
+ 
+
+//second Class
+
+class Persone_2 {
+public $name;
+public $age;
+public $email;
+public $sallery;
+public function userDataFunction() {
+return $this-> age + $this->sallery;
+}
 };
 
-$ahmed->name="ahmed";
-var_dump( $ahmed->name);
+$userData1=new Persone_2;
+$userData1 -> name="Ahmed";
+$userData1->age=25;
+$userData1->sallery=10;
+
+echo $userData1->userDataFunction() . "<br>";
+echo $userData1->name;
+
+// Constructor
+
+class Student {
+public $name;
+public $age;
+public $level;
+public $score;
+public $maxSore;
+public $minScore;
+public $subject=["A"=>"0","E"=>"0","M"=>"0"];
+public function __construct($name,$age)
+  {
+    $this->name=$name;
+    $this->age=$age;
+  }
+}
+
+
+
+
+$student1=new Student("abdallh",10);
